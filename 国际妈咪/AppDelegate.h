a9da0e353ff5861f34fba840167db4267718966f
Jerry Huang <495658492@qq.com>
2018-03-23 00:59:52 +0800

    Initial Commit

---
+++ 国际妈咪/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  国际妈咪
+//
+//  Created by Jerry Huang on 2018/3/23.
+//  Copyright © 2018年 Jerry Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 国际妈咪/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  国际妈咪
+//
+//  Created by Jerry Huang on 2018/3/23.
+//  Copyright © 2018年 Jerry Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
